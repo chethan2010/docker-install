c1a3ba05ec585364a0751828b7cee6fbbe0a8a1d
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-08-06 07:33:27 +0530

    docker

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "db" {
     ami ="ami-09c813fb71547fc4f"
     vpc_security_group_ids = var.security_group_ids
     instance_type = var.instance_type
-    user_data=file("docker.sh")
+    user_data = file("docker.sh")
 
     tags = {
-        Name = "db"
+        Name = "docker"
     }
 }
--- variables.tf
+++ variables.tf
@@ -12,14 +12,7 @@
 
     variable "security_group_ids" {
     type = list
-    default = ["sg-0bacc2172c8d3eb1b"] #replace with your SG ID.
+    default = ["sg-0bf7eae1971a8cf1c"] #replace with your SG ID.
     }
 
-    variable "tags" {
-        default = {
-        projects = "Expense"
-        module  = "db" 
-        name = "db"
-             
-        } 
-    }
+   
